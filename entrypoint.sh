822b53b8d6bc9901e35ac2ffae598ad886d61bce
Lucas Van Mol <lucas.vanmol@gmail.com>
2021-12-17 13:38:44 -0800

    Fix contains()

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,8 @@
 
 configs="release beta dev nightly"
 
-contains() {
-    [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && exit(0) || exit(1)
-}
+contains() { [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && exit(0) || exit(1); }
 
-echoerr() { printf "%s\n" "$*" >&2; }
 
 contains configs $1
 if [ $? -eq 0 ]; then
