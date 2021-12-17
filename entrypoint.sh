0d45a1acafd65e442500c2434ca920a566f38d3c
Lucas Van Mol <lucas.vanmol@gmail.com>
2021-12-17 13:43:10 -0800

    Replace contains()

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,11 @@
 #!/bin/sh -l
 
-configs="release beta dev nightly"
-
-contains() { [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && exit(0) || exit(1); }
-
-
-contains configs $1
-if [ $? -eq 0 ]; then
+if [[ "$1" =~ ^(release|beta|dev|nightly)$ ]]; then
     git clone https://github.com/algorand/sandbox.git
     cd sandbox
     ./sandbox up $1 -v
     curl --fail --show-error localhost:4001/health -H "X-Algo-API-Token: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 else
-    printf "Invalid config. Must be one of: %s\n" "$configs" >&2;
+    printf "Invalid config. Must be one of: relase/beta/dev/nightly" "$configs" >&2;
     exit 1
 fi
-
