18593cb96422bf249b1dfd7060d2d154cbc514cd
Lucas Van Mol <lucas.vanmol@gmail.com>
2021-12-17 14:18:50 -0800

    Move curl test to workflows

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@ if [[ "$1" =~ ^(release|beta|dev|nightly)$ ]]; then
     git clone https://github.com/algorand/sandbox.git
     cd sandbox
     ./sandbox up $1 -v
-    sleep 1
-    curl --fail --show-error localhost:4001/health -H "X-Algo-API-Token: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    exit 0
 else
     printf "Invalid config. Must be one of: relase/beta/dev/nightly" "$configs" >&2;
     exit 1
